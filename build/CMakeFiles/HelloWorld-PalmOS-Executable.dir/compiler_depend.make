180d9e0221cd863ea597cde74c65bd27774efad9
Spencer Colton <me@scolton.tech>
2026-08-16 16:14:38 -0500

    Remove build dir

--- build/CMakeFiles/HelloWorld-PalmOS-Executable.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for HelloWorld-PalmOS-Executable.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Resource-HelloWorld.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Resource-HelloWorld"
-  "HelloWorld.ro"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Resource-HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
